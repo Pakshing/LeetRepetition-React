5875d749c76671347581832202f846910b9e3b84
Pak Shing Kan <kennethkankps@gmail.com>
2024-03-20 21:28:53 -0400

    fixed color schema for category

--- src/utils/TableItemColors.ts
+++ src/utils/TableItemColors.ts
@@ -1,27 +1,22 @@
 // create a function that takes a string, category (like leetcode different tags) and returns a color
 
 export const tagColors = (tag: string) => {
-    if(tag.toLowerCase() === 'array') return 'geekblue';
-    if(tag.toLowerCase() === 'string') return 'green';
-    if(tag.toLowerCase() === 'linkedlist') return 'volcano';
-    if(tag.toLowerCase() === 'tree') return 'orange';
-    if(tag.toLowerCase() === 'graph') return 'gold';
-    if(tag.toLowerCase() === 'dynamic programming') return 'cyan';
-    if(tag.toLowerCase() === 'backtracking') return 'purple';
-    if(tag.toLowerCase() === 'math') return 'magenta';
-    if(tag.toLowerCase() === 'bit manipulation') return 'lime';
-    if(tag.toLowerCase() === 'greedy') return 'blue';
-    if(tag.toLowerCase() === 'stack') return 'red';
-    if(tag.toLowerCase() === 'queue') return 'yellow';
-    if(tag.toLowerCase() === 'heap') return 'gold';
-    if(tag.toLowerCase() === 'sort') return 'cyan';
-    if(tag.toLowerCase() === 'binary search') return 'purple';
-    if(tag.toLowerCase() === 'hash table') return 'magenta';
-    if(tag.toLowerCase() === 'two pointers') return 'lime';
+    if(tag.toUpperCase() === 'ARRAY') return 'geekblue';
+    if(tag.toUpperCase() === 'STRING') return 'green';
+    if(tag.toUpperCase() === 'LINKED_LIST') return 'volcano';
+    if(tag.toUpperCase() === 'HASHING') return 'orange';
+    if(tag.toUpperCase() === 'HEAP') return 'gold';
+    if(tag.toUpperCase() === 'SLIDING_WINDOW') return 'cyan';
+    if(tag.toUpperCase() === 'STACK&QUEUE') return 'purple';
+    if(tag.toUpperCase() === 'TREE') return 'magenta';
+    if(tag.toUpperCase() === 'GRAPH') return 'lime';
+    if(tag.toUpperCase() === 'DP') return 'blue';
+    if(tag.toUpperCase() === 'GREEDY') return 'red';
+    if(tag.toUpperCase() === 'SORTING') return 'yellow';
+    if(tag.toUpperCase() === 'BACKTRACKING') return 'gold';
 
     return 'geekblue';
 };
-
 
 export const difficultyColors = (difficulty: string) => {
     if(difficulty.toLowerCase() === 'easy') return 'green';
