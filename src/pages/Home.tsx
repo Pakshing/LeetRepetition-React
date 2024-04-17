365008e79a2da6a612efd33487ec1948e2965a0f
Pak Shing Kan <kennethkankps@gmail.com>
2024-04-17 16:18:35 -0400

    home page using Cookie to determine redirect or login

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,9 +3,10 @@ import { Button } from 'antd'
 import LoginModal from '../components/Modal/LoginModal'
 import './Home.module.css'
 import logo from '../assets/logo.png'
+import Cookies from 'js-cookie'
 
 const Home = () => {
-
+    
     return (
         <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center', height: '75vh', padding: '1rem', marginTop:'1rem' , backgroundColor: "#1A1F2B", }}>
             <div style={{display:'flex', justifyContent:'center' , alignItems:'center', flexDirection: 'column', flex: 1}}>
@@ -13,7 +14,7 @@ const Home = () => {
                     <h1>Leetcode Scheduler</h1>
                     <p style={{lineHeight:'2',marginTop:'3rem', marginBottom:'3rem'}}>Introducing Leetcode Scheduler! This application facilitates the tracking of your Leetcode question completions and offers scheduling based on spaced-repetition learning techniques to optimize your learning process. Embark on your journey to Leetcode mastery today!</p>
                 </div>
-                {localStorage.getItem("user_email") && localStorage.getItem("user_id") ?  <Button size='large'><a href='/question'><b>Let's grind!</b> </a></Button> : <LoginModal/>}
+                {Cookies.get('token') ?  <Button size='large'><a href='/question'><b>Let's grind!</b> </a></Button> : <LoginModal/>}
             </div>
             
             <div style={{ flex: 1,marginRight:'2rem' }}>
